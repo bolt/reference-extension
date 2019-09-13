f15927b23d5567dbb281e92749ab788770a0f1f4
Bob den Otter <bob@twokings.nl>
2019-09-13 16:05:02 +0200

    Updating eventlistener

--- src/EventListener.php
+++ src/EventListener.php
@@ -4,10 +4,15 @@ declare(strict_types=1);
 
 namespace AcmeCorp\ReferenceExtension;
 
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+
 class EventListener
 {
-    public function handleEvent($e): void
+    public function handleEvent(ResponseEvent $e): void
     {
-        echo '<h2>It Works!</h2>';
+        $content = $e->getResponse()->getContent();
+        $content .= "\n<!-- It works! -->\n";
+
+        $response->setContent($content);
     }
 }
